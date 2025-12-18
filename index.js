526acc08e76dd87062c7b74bda1622a11431cc34
arnobroys <arnobroy066@gmail.com>
2025-12-18 21:57:59 +0600

    navbar size fixing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var typed= new Typed(".text", {
-    strings:["C S E Student"],
-    typeSpeed: 100,
-    backSpeed: 100,
+    strings:["C S E   Student"],
+    typeSpeed: 130,
+    backSpeed: 90,
     backDelay: 1000,
     loop:true
 });
